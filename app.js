89a81d5a575f9770662c7f5e9ae13c2a6cbec26a
Harsh Gajera <harsh.gajera17@gmail.com>
2022-05-05 21:53:22 +0530

    done

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 var cors = require("cors");
 
+const Razorpay = require("razorpay");
+const bodyParser = require("body-parser");
+
 const productRouter = require("./routes/productRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -14,10 +17,71 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   req.time = new Date().toLocaleString();
   console.log(req.time);
   next();
+});
+
+// ******************************** //
+
+const razorpay = new Razorpay({
+  key_id: "rzp_test_OnubQmqY8GahSs",
+  key_secret: "I7b8J5m7VeCfcpWNi1lqev2f",
+});
+
+app.post("/verification", (req, res) => {
+  // do a validation
+  const secret = "12345678";
+
+  console.log(req.body);
+
+  const crypto = require("crypto");
+
+  const shasum = crypto.createHmac("sha256", secret);
+  shasum.update(JSON.stringify(req.body));
+  const digest = shasum.digest("hex");
+
+  console.log(digest, req.headers["x-razorpay-signature"]);
+
+  if (digest === req.headers["x-razorpay-signature"]) {
+    console.log("request is legit");
+    // process it
+    require("fs").writeFileSync(
+      "payment1.json",
+      JSON.stringify(req.body, null, 4)
+    );
+  } else {
+    // pass it
+  }
+  res.json({ status: "ok" });
+});
+
+app.post("/razorpay", async (req, res) => {
+  const payment_capture = 1;
+  const amount = 1600;
+  const currency = "INR";
+
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: "xD1T-EA8-",
+    payment_capture,
+  };
+
+  try {
+    const response = await razorpay.orders.create(options);
+    console.log(response);
+    res.json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // ******************************** //
